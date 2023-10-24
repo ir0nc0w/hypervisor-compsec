c939a5412e2e79a60163ca6f2523b853720406ef
ir0nc0w <cwmyung@snu.ac.kr>
2023-10-24 13:56:42 +0900

    upadte: added build scripts

---
+++ bareflank/build/packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+# Install prerequisite packages
+sudo apt install git build-essential nasm clang cmake libelf-dev
---
+++ bareflank/build/reset.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+rm -rf cmake_install.cmake CMake* Makefile bf* cpuidcount/ export/ rdtsc/ vpid/ hook/ prefixes/ depends/ setup_git_dir.sh
